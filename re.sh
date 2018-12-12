32a19cf5e82dd0fed881edbfa3dc9bcf8df367dd
Caleb Nordgren <cdn841@gmail.com>
2018-12-12 12:06:37 -0600

    Finished Lab version of re.sh

--- re.sh
+++ re.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CONFIG="$1"
 COMMAND="$2"
+CONFIG2="/etc/apache2/sites-available/"
 
 if [ $# -ne 2 ]
 then
@@ -8,21 +9,36 @@ then
     exit 1
 fi
 
-# only allow reload or restart
-if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
-then
-    # Move the current execution state to the proper directory
-    cd /etc/apache2/sites-available
+VHOSTS_PATH=/etc/apache2/sites-available/*.conf
 
-    # Disable a vhost configuration
-    sudo a2dissite "$CONFIG"
-    sudo service apache2 "$COMMAND"
+for FILENAME in $VHOSTS_PATH
+do 
+    if [ "$CONFIG2$CONFIG" == "$FILENAME" ]
+    then 
+        # only allow reload or restart
+        if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
+        then
+            # Move the current execution state to the proper directory
+            cd /etc/apache2/sites-available
 
-    # Enable a vhost configuration
-    sudo a2ensite "$CONFIG"
-    sudo service apache2 "$COMMAND"
+            # Disable a vhost configuration
+            sudo a2dissite "$CONFIG"
+            sudo service apache2 "$COMMAND"
 
-else
-    echo "ERROR $COMMAND is an invalid service command {restart|reload}"
-    exit 1
-fi
+            # Enable a vhost configuration
+            sudo a2ensite "$CONFIG"
+            sudo service apache2 "$COMMAND"
+            exit 1
+
+        else
+            echo "ERROR $COMMAND is an invalid service command {restart|reload}"
+            exit 1
+        fi
+    fi
+done
+
+echo "ERROR: Invalid virtual-host name. Valid host names:"
+for FILENAME1 in $VHOSTS_PATH
+do
+    echo "$FILENAME1"
+done
